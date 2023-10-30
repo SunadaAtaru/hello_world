9adac7daa0ad0f2ee33ff0610204333b4fa8a4c5
SunadaAtaru <sleepingdoll2008@gmail.com>
2023-10-30 23:29:52 +0900

    devise導入編

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  
+  devise_for :users
+  root to: 'homes#top' #これで、homesコントローラー#topアクションを探しに行く
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  
 end
